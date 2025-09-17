9743f68dbfef7bf6969b635de852b56c71de9f8f
Matheus <matheusfreitas1402@gmail.com>
2025-09-16 23:13:41 -0300

    update method post and get in memory

--- src/main/java/com/example/shipping/controllers/OrderController.java
+++ src/main/java/com/example/shipping/controllers/OrderController.java
@@ -10,17 +10,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/orders")
 public class OrderController {
 
+    private Order lastOrder;
+
     @Autowired
     private OrderService orderService;
+
 
     @PostMapping("/calculate")
     public OrderTotalDTO calculateTotal(@RequestBody Order order) {
         double total = orderService.total(order);
+        this.lastOrder = order; // guarda o pedido em memória
         return new OrderTotalDTO(order.getCode(), total);
     }
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "API de frete funcionando!";
+
+    @GetMapping("/lastOrder")
+    public Order getLastOrder() {
+        if (lastOrder == null) {
+            throw new RuntimeException("Nenhum pedido foi cadastrado ainda!");
+        }
+        return lastOrder;
     }
 }
+
